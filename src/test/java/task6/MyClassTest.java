bd8c3e47a6b0e6b5e4ec19594d43290ab2058d0e
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2023-10-02 15:07:17 +0300

    syntactic corrections task6

--- src/test/java/task6/MyClassTest.java
+++ src/test/java/task6/MyClassTest.java
@@ -13,13 +13,13 @@ public class MyClassTest {
     public void isSumListCorrect() {
         List<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3));
         List<Integer> list2 = new ArrayList<>(Arrays.asList(1,2,3));
-        List<Integer> result = new ArrayList<>(Arrays.asList(2,4,5));
+        List<Integer> result = new ArrayList<>(Arrays.asList(2,4,6));
         List<Integer> resultTest = MyClass.sumLists(list1, list2);
-        assertEquals(result, resultTest, "Сумма списков не верна");
+        assertArrayEquals(result.toArray(), resultTest.toArray(), "Сумма списков не верна");
     }
 
     @Test
-    public  void isFinndMaxCorrect() {
+    public  void isFindMaxCorrect() {
         List<Integer> list1 = new ArrayList<>(Arrays.asList(1,2,3));
         int result = 3;
         int resultTest = MyClass.findMax(list1);
@@ -30,7 +30,8 @@ public class MyClassTest {
     public void isFilterStringsCorrect() {
         List<String> list1 = new ArrayList<>(Arrays.asList("Я", "говорю", "привет"));
         List<String> result = new ArrayList<>(Arrays.asList("говорю", "привет"));
-        List<String> resultTest = MyClass.filterStrings(list1, 5);
+        int minLength = 5;
+        List<String> resultTest = MyClass.filterStrings(list1, minLength);
         assertEquals(result, resultTest, "Фильтрация не корректна");
     }
 }
